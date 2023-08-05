4cb06c8ea3891c6ff4ba197a3893cab4a2082684
Abdulmuiz mustapha <reezy77jay@gmail.com>
2023-08-05 17:36:27 +0100

    eslinc changed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,21 +13,21 @@ const Page = () => {
         <div className={styles.item}>
           <h1 className={styles.title}>Who am I?</h1>
           <p className={styles.desc}>
-            Hey! I’m Abdulmuiz Mustapha, a creative. Since a kid I have loved
-            art, sketching was my favourite hobby. My family were heavily
+            Hey! I&apos;m Abdulmuiz Mustapha, a creative. Since a kid I have
+            loved art, sketching was my favourite hobby. My family were heavily
             education minded which meant in 2018 I came to the UK to study at
             University; unfortunately it was for a subject I had no passion for.
-            I carried on through University but was miserable. I wasn’t doing
-            well in my exams and started getting depressed. In 2021, I realised
-            my life needed to change, I dropped out of University and I started
-            studying Web Development. My life saw a huge turning point. I
-            started to feel myself coming back through the creative processes of
-            designing digital pieces and constructing websites and applications
-            using multiple coding languages. I later studied Adobe Suite and
-            took a graphic design course, where my passion for branding and
-            digital art was truly unleashed. I pride myself in now letting my
-            creative mind and soul free, manifesting and executing ideas in to
-            reality.
+            I carried on through University but was miserable. I wasn&apos;t
+            doing well in my exams and started getting depressed. In 2021, I
+            realised my life needed to change, I dropped out of University and I
+            started studying Web Development. My life saw a huge turning point.
+            I started to feel myself coming back through the creative processes
+            of designing digital pieces and constructing websites and
+            applications using multiple coding languages. I later studied Adobe
+            Suite and took a graphic design course, where my passion for
+            branding and digital art was truly unleashed. I pride myself in now
+            letting my creative mind and soul free, manifesting and executing
+            ideas in to reality.
           </p>
         </div>
 
